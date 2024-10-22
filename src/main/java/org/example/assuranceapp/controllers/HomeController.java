933c1c510d57be2edc206dd693a1c228ae2b973b
MahaDaiki <mahadaiki2@gmail.com>
2024-10-22 09:38:47 +0100

    Initial commit Configuration de l'envirenment

---
+++ src/main/java/org/example/assuranceapp/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.assuranceapp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the Assurance App!");
+        return "home";
+    }
+}
---
+++ src/main/webapp/views/home.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Maha
+  Date: 10/22/2024
+  Time: 11:36 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Assurance App</title>
+</head>
+<body>
+<h1>${message}</h1>
+</body>
+</html>
